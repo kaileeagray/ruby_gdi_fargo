fc9b9efa4626249667a8bbf52ce4e7873652b053
Kailee <kaileeagray@gmail.com>
2017-05-15 15:46:46 -0500

    update class 1

---
+++ name.rb
@@ -0,0 +1,7 @@
+puts 'Hello there, and what\'s your name?'
+name = gets
+puts 'Your name is ' + name.chomp + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
+puts 'Hello there, and what\'s your age, in years?'
+age = gets.chomp
+puts 'Thank you, ' + name + '. You are ' + age + ' years old'
